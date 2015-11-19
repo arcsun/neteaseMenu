da69a25a07cb51a0a1949d33333edada793ca769
zy <609885234@qq.com>
2015-11-19 12:40:09 +0800

    初始化

--- codepy/menu.py
+++ codepy/menu.py
@@ -7,11 +7,14 @@ import menulog
 urlhead = 'http://numenplus.yixin.im/singleNewsWap.do?materialId='
 frequency = 3600
 
+
 class Menu:
     def __init__(self, day= 0):
         self.today = int(time.strftime('%y%m%d',time.localtime(time.time())))  # 151022
         if day == 1:
             self.today += 1
+        elif day == 0 or 100:
+            pass
         elif day != 0 and day > 151026 :
             self.today = day
 
@@ -22,6 +25,13 @@ class Menu:
         self.cache = {}         # {151019:15163}  # 日期:id
         self.maybe = []         # 爬到的报错的页面
 
+        if day == 100:
+            self.returnMaybe = True
+        else:
+            self.returnMaybe = False
+        self.maybeUrl = ''
+
+
     def process(self):
         def getTime():
             return time.strftime('20%y-%m-%d %H:%M:%S', time.localtime())
@@ -31,6 +41,8 @@ class Menu:
 
         def getMaybe():
             backup = copy.deepcopy(self.maybe)
+            backup.sort()
+            backup.reverse()
             self.result += u'\t 可能的url: '
             first = True
             for mid in backup:
@@ -38,6 +50,8 @@ class Menu:
                     if first:
                         first = False
                         self.result += u'\t' + urlhead+ str(mid)
+                        if self.returnMaybe:
+                            self.maybeUrl = urlhead+ str(mid)
                     else:
                         self.result += '\t' + str(mid)
                 else:
@@ -65,9 +79,15 @@ class Menu:
             return getUrl()
         else:
             # 缓存中查不到
+            print self.returnMaybe
+
+
             menulog.info('cache not found @%s'% getTime())
             if self.result == u'未找到菜单':
                 getMaybe()
 
-            self.result += u'\t下次刷新:约%d秒后'% (self.lastQuery + frequency - self.now)
-            return self.result
+            if self.returnMaybe and self.maybeUrl:
+                return self.maybeUrl
+            else:
+                self.result += u'\t下次刷新:约%d秒后'% (self.lastQuery + frequency - self.now)
+                return self.result
