89c70cdeb739baded25531c8959b631268f00b25
sun <60995234@qq.om>
2016-10-23 15:06:52 +0800

    一大波更新

--- restart.sh
+++ restart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ps -ef | grep gunicorn | awk '{print $2}' | xargs kill -9
-ps -ef | grep bgtask.py | awk '{print $2}' | xargs kill -9
-
+ps -ef | grep bgtask.py | awk '{print $2}' | xargs kill -9¡¿
 sleep 2
 
 (setsid python bgtask.py &)
