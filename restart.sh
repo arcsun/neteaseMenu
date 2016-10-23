f408cb73bc946fa9bacaf96f197be41d4f4353d1
sun <60995234@qq.om>
2016-10-23 15:29:12 +0800

    一大波更新

--- restart.sh
+++ restart.sh
@@ -5,4 +5,4 @@ ps -ef | grep bgtask.py | awk '{print $2}' | xargs kill -9
 sleep 2
 
 (setsid python bgtask.py &)
-gunicorn -b 0.0.0.0:5000 -k gevent start:app
+gunicorn -b 0.0.0.0:80 -k gevent start:app
