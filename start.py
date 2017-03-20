4d2aa7551342189bf8df50e8d6c4080f85f244d7
sun <609885234@qq.com>
2017-03-20 15:30:30 +0800

    fix

--- start.py
+++ start.py
@@ -355,7 +355,7 @@ def readLog(lines= 0):
 # temp
 @app.route('/api/v1/verify', methods=['POST', 'GET'])
 def mockYidun():
-    return "true"
+    return '{"result":true, "error":"", "msg":""}'
 
 
 if __name__ == '__main__':
